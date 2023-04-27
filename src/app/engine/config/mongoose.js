9ad228428ec4eff975f6c1a0c4e7e02315c1551e
Sumit Yadav <sumityadav0376@gmail.com>
2023-04-27 18:25:28 +0530

    Added mongodb atlas configuration

--- src/app/engine/config/mongoose.js
+++ src/app/engine/config/mongoose.js
@@ -1,16 +1,35 @@
+// Cloud DB
 const mongoose = require('mongoose');
 const env = require('./env');
 
-mongoose.connect(`mongodb://localhost/${env.db}`, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose
+  .connect(env.mongodbAtlasUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB cloud');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
 
 
-db.once('open', function(){
-    console.log('Connected to Database :: MongoDB');
-});
+//   // Local DB
+// const mongoose = require('mongoose');
+// const env = require('./env');
+
+// mongoose.connect(`mongodb://localhost/${env.db}`, {useNewUrlParser: true, useUnifiedTopology: true});
+
+// const db = mongoose.connection;
+
+// db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
 
 
-module.exports = db;
+// db.once('open', function(){
+//     console.log('Connected to Database :: MongoDB');
+// });
+
+
+// module.exports = db;
