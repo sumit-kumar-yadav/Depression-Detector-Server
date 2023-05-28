0e327bdaf7bfba23e5375d919f4515b42b80ea91
Sumit Yadav <sumityadav0376@gmail.com>
2023-05-29 03:28:04 +0530

    To show image at specific location use - <img src="cid:report">

--- src/app/emails/mailers/report.js
+++ src/app/emails/mailers/report.js
@@ -13,6 +13,7 @@ const sendReportToClient = (doctor, client, report) => {
         attachments: [{
             filename: 'report.jpg',
             content: report.file, // Attach the buffer directly
+            cid: 'report', // Use the same unique identifier as in the HTML image source
             contentDisposition: 'attachment',
         }]
     }, (err, info) => {
